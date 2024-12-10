22700c7ccb67989f9962e6fbf1c0413fdc9a8adc
Elektro133x <elektro9034@gmail.com>
2024-12-10 04:16:51 +0300

    Добавляет функционал удаления

--- TaskManager.cs
+++ TaskManager.cs
@@ -17,4 +17,16 @@ public class TaskManager
             Console.WriteLine("Задача не может быть пустой.");
         }
     }
+
+    public void RemoveTask(int index)
+    {
+        if (index >= 0 && index < tasks.Count) {
+            tasks.RemoveAt(index);
+            Console.WriteLine("Задача удалена!");
+        }
+        else
+        {
+            Console.WriteLine("Неверный индекс.");
+        }
+    }
 }
